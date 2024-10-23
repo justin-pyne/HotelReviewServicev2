8a4745210e49751abb5fe6703988f12179054ea4
justin-pyne <jpyne.justin@gmail.com>
2024-10-23 14:02:11 -0700

    Modifying recursive traversal of JSON directories from Project 1 to submit threads to poolManager and register with phaser

--- src/main/java/hotelapp/Service/JsonService.java
+++ src/main/java/hotelapp/Service/JsonService.java
@@ -7,9 +7,11 @@ import com.google.gson.JsonParser;
 import hotelapp.Model.Hotel;
 import hotelapp.Model.Review;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
@@ -17,13 +19,58 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Phaser;
 
 public class JsonService {
-    private ExecutorService executor;
+    private ExecutorService poolManager;
     private Phaser phaser = new Phaser();
     private String output;
+    private JsonParser parser = new JsonParser();
 
     public JsonService(int numThreads, String output) {
         this.output = output;
+        this.poolManager = Executors.newFixedThreadPool(numThreads);
     }
+
+    class ReviewWorker implements Runnable {
+        private File file;
+        private List<Review> localReviews = new ArrayList<>();
+        public ReviewWorker(File file, List<Review> reviews) {
+            this.file = file;
+        }
+
+        @Override
+        public void run() {
+            try(BufferedReader br = new BufferedReader(new FileReader(file))) {
+                JsonObject obj = (JsonObject)parser.parse(br);
+                JsonObject reviewDetails = obj.getAsJsonObject("reviewDetails");
+                JsonObject reviewCollection = reviewDetails.getAsJsonObject("reviewCollection");
+                JsonArray reviewArray = reviewCollection.getAsJsonArray("review");
+
+                for (JsonElement ele : reviewArray) {
+                    JsonObject reviewObj = ele.getAsJsonObject();
+
+                    String hotelId = reviewObj.get("hotelId").getAsString();
+                    String reviewId = reviewObj.get("reviewId").getAsString();
+                    int rating = reviewObj.get("ratingOverall").getAsInt();
+                    String userNickname = reviewObj.get("userNickname").getAsString();
+                    String title = reviewObj.get("title").getAsString();
+                    String reviewText = reviewObj.get("reviewText").getAsString();
+                    String date = reviewObj.get("reviewSubmissionDate").getAsString();
+
+                    Review review = new Review(hotelId, reviewId, rating, title, reviewText, userNickname, date);
+                    localReviews.add(review);
+                }
+                addReviews(reviews);
+            } catch(IOException e) {
+                System.out.println(e);
+            } finally {
+                phaser.arriveAndDeregister();
+            }
+        }
+    }
+
+    private synchronized void addReviews(List<Review> reviews, List<Review> localReviews) {
+        reviews.addAll(localReviews);
+    }
+
 
     public List<Hotel> parseHotel(String filePath) {
         List<Hotel> hotels = new ArrayList<>();
@@ -57,8 +104,27 @@ public class JsonService {
     }
 
     public List<Review> parseReviews(String filePath) {
+        List<Review> reviews = new ArrayList<>();
 
+        traverseReviewDirectory(filePath, reviews);
+
+        return reviews;
     }
 
+    private void traverseReviewDirectory(String filePath, List<Review> reviews) {
+        Path p = Paths.get(filePath);
+        try(DirectoryStream<Path> pathsInDir = Files.newDirectoryStream(p)) {
+            for(Path path : pathsInDir) {
+                if(Files.isDirectory(path)) {
+                    traverseReviewDirectory(path.toString(), reviews);
+                } else if (path.toString().endsWith(".json")) {
+                    poolManager.submit(new ReviewWorker(path.toFile(), reviews));
+                    phaser.register();
+                }
+            }
+        } catch(IOException e) {
+            System.out.println(e);
+        }
+    }
 
 }
